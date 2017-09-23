9180a4c5eaa324acf1cd973b68ac1346bab1cebe
Stanley Rokita <srok35@gmail.com>
2017-09-23 14:47:53 -0400

    get all lists route now gives jsonify function proper array of json objects

--- models/list.py
+++ models/list.py
@@ -4,3 +4,8 @@ from .base_model import BaseModel
 class List(BaseModel):
 
 	name = CharField(max_length=150)
+
+	def to_dict_with_public_data(self):
+		return {
+			'name': self.name
+		}
--- routes/route_list.py
+++ routes/route_list.py
@@ -3,12 +3,14 @@ from models.list_user import *
 
 def get_route_list_all():
 
-    phone_num = g.user.get('user_name')
+    phone_num = g.user.get('phone_num')
 
     succeed, lists, err_msg = fetch_lists_for_phone_num(phone_num)
 
+    lists_to_return = [ list.to_dict_with_public_data() for list in lists ]
+
     if succeed:
-        return jsonify({'success': True, 'lists': lists})
+        return jsonify({'success': True, 'lists': lists_to_return})
 
     else:
         return jsonify({'success': False, 'errMsg': err_msg})
