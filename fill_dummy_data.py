895927e40354f225c8d4c48824b9b450c6906265
Stanley Rokita <srok35@gmail.com>
2017-09-23 14:07:25 -0400

    Procfile run fill_dummy and gunicorn on same line

--- fill_dummy_data.py
+++ fill_dummy_data.py
@@ -20,12 +20,12 @@ def fill_dummy_items():
     for dumb_item in dummy_items:
         print('Adding Item:', dumb_item)
         Item.create(text=dumb_item.get('text'), is_checked=dumb_item.get('is_checked'),
-                    list_id=dumb_item.get('list'))
+                    list=dumb_item.get('list'))
 
 def fill_dummy_list_users():
     for dumb_list_user in dummy_list_users:
         print('Adding ListUser:', dumb_list_user)
-        ListUser.create(list_id=dumb_list_user.get('list'), user_id=dumb_list_user.get('user'))
+        ListUser.create(list=dumb_list_user.get('list'), user=dumb_list_user.get('user'))
 
 def run():
     fill_dummy_users()
